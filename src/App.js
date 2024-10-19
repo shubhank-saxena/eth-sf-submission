f87c67667b1bd599589584b3e57600862119a002
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2024-10-19 11:38:33 -0700

    Starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
+import Main from "./Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <DynamicContextProvider
+    theme="auto"
+    settings={{
+      environmentId: "f035d446-74a5-48b6-b705-1616a7dffab1",
+      walletConnectors: [EthereumWalletConnectors, ZeroDevSmartWalletConnectors],
+    }}
+  >
+    <Main />
+  </DynamicContextProvider>
+);
 
 export default App;
